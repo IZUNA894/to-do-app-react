e329b94ae32857cc8a0017061934c4a9e2b7c0cb
IZUNA894 <48329138+IZUNA894@users.noreply.github.com>
2020-03-25 16:11:22 +0530

    Updates

---
+++ precache-manifest.a984a0982e4d1bf5ef44402c04dc8e2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ca284633fa7025c177a324dead180f1",
+    "url": "/IZUNA894/index.html"
+  },
+  {
+    "revision": "3181b28b928ae875f1fe",
+    "url": "/IZUNA894/static/css/main.39a3162b.chunk.css"
+  },
+  {
+    "revision": "f93fda5ea6284de3ffbc",
+    "url": "/IZUNA894/static/js/2.952dfd10.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/IZUNA894/static/js/2.952dfd10.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3181b28b928ae875f1fe",
+    "url": "/IZUNA894/static/js/main.3ae54917.chunk.js"
+  },
+  {
+    "revision": "253176e6ab79e13d829e",
+    "url": "/IZUNA894/static/js/runtime-main.4b4c999f.js"
+  }
+]);
